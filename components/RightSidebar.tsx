203dba5e61e90285852e690d6dabf244aa62dd0f
mkaidev <mkaidev88@gmail.com>
2024-06-01 23:08:15 +0700

    homepage layout

---
+++ components/RightSidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RightSidebar = () => {
+  return (
+    <section className="right_sidebar h-[calc(100vh-5px)] text-white-1">
+      RightSidebar
+    </section>
+  );
+};
+
+export default RightSidebar;
