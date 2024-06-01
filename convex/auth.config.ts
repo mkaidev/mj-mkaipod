6a7dc309961339fc1e0ec5063bdff44830fa6767
mkaidev <mkaidev88@gmail.com>
2024-06-02 03:21:32 +0700

    convex & clerk setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://amusing-boar-26.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
